318971ca636cc96204a57ed91f2ef2525eaac1ef
mrjoelee <joe.05.lee@outlook.com>
2022-12-18 16:54:29 -0500

    updated toggle on/off mode

--- js/day-night.js
+++ js/day-night.js
@@ -1,7 +1,9 @@
 //path (d) value of moon and sun emoji
 // the path of sun is morphed to moon and vice versa
-const moonPath = "M16 27.5C16 42.6878 27.5 52.5 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 16 12.3122 16 27.5Z";
-const sunPath= "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
+const moonPath =
+  "M16 27.5C16 42.6878 27.5 52.5 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C27.5 0 16 12.3122 16 27.5Z";
+const sunPath =
+  "M55 27.5C55 42.6878 42.6878 55 27.5 55C12.3122 55 0 42.6878 0 27.5C0 12.3122 12.3122 0 27.5 0C42.6878 0 55 12.3122 55 27.5Z";
 
 //select svg
 const darkMode = document.querySelector("#darkMode");
@@ -9,33 +11,42 @@ const darkMode = document.querySelector("#darkMode");
 let toggle = false;
 
 // We need to click on the sun
-darkMode.addEventListener('click', ()=> {
+darkMode.addEventListener("click", () => {
   //We need to animate using anime.js
   //Here we set the TIMELINE
   const timeline = anime.timeline({
     duration: 300,
-    easing: "easeOutExpo"
+    easing: "easeOutExpo",
   });
   //Add different animations to the timeline
-  timeline.add({
-    targets: ".sun",
-    d: [{ value: toggle ? sunPath: moonPath }]   
-  })
-  .add({
-    targets: "#darkMode",
-    rotate: toggle ? 0 : 320
-  }, '-= 150')   // -= is used for duration
-  .add({
-    targets: "body",
-    backgroundColor: toggle ? 'rgb(240,240,240)' : "rgb(60,60,80)",
-    color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)"
-  }, '-= 150')
-  .add({
-    targets: ["nav", "section", ".navbar-brand"],
-    backgroundColor: toggle ? 'rgb(60,60,80)' : 'rgb(240,240,240)',
-    color: toggle ? 'rgb(255,255,255)' : 'rgb(22,22,22)'
-  }, '-= 150');
+  timeline
+    .add({
+      targets: ".sun",
+      d: [{ value: toggle ? sunPath : moonPath }],
+    })
+    .add(
+      {
+        targets: "#darkMode",
+        rotate: toggle ? 0 : 320,
+      },
+      "-= 150"
+    ) // -= is used for duration
+    .add(
+      {
+        targets: "body",
+        backgroundColor: toggle ? "rgb(255,255,255)" : "rgb(22,22,22)",
+        color: toggle ? "rgb(22,22,22)" : "rgb(255,255,255)",
+      },
+      "-= 150"
+    )
+    .add(
+      {
+        targets: ["nav", "section", ".navbar-brand"],
+        backgroundColor: toggle ? "rgb((255,255,255)" : "rgb(108, 117, 125)",
+        color: toggle ? "rgb(255,255,255)" : "rgb(22,22,22)",
+      },
+      "-= 150"
+    );
   //Everytime we click on the sun toggle switches from true to false
   toggle = !toggle;
 });
-
