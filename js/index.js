b7558b0e40a4efbc095310dc972db1d8059a295b
mrjoelee <joe.05.lee@outlook.com>
2022-12-17 20:35:07 -0500

    axios, refactor code

--- js/index.js
+++ js/index.js
@@ -1,88 +1,95 @@
-const options = {
-  method: "GET",
-  headers: {
-    "X-RapidAPI-Key": "56fc230643msh7620609da5ab5a7p15eba2jsn84dde9e32f2d",
-    "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
-  },
-};
+//Get by Title Movies
+document
+  .getElementById("search-title")
+  .addEventListener("submit", handleAddSubmitTitle);
 
-// const searchBtn = document.getElementById("search");
-// searchBtn.addEventListener("submit", handleAddSubmit);
+document
+  .getElementById("search-keyboard")
+  .addEventListener("submit", handleAddSubmitKeyboard);
 
-try {
-  document
-    .getElementById("search-form")
-    .addEventListener("submit", handleAddSubmit);
+function handleAddSubmitTitle(event) {
+  event.preventDefault();
 
-  async function handleAddSubmit(event) {
-    event.preventDefault();
+  const inputTitle = document.getElementById("title-input").value;
+  //clears the forms after it hit searches
+  document.getElementById("search-title").reset();
+  const optionsTitle = {
+    method: "GET",
+    url: "https://streaming-availability.p.rapidapi.com/v2/search/title",
+    params: {
+      title: `${inputTitle}`,
+      country: "us",
+      type: "all",
+      output_language: "en",
+    },
+    headers: {
+      "X-RapidAPI-Key": "fa0586e4cfmsh7f4e00de64e8f8cp134e8ajsn1a8ff941bcbc",
+      "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+    },
+  };
 
-    const inputValue = document.getElementById("search-input").value;
+  axios
+    .request(optionsTitle)
+    .then(function (response) {
+      const dataTitle = response.data;
+      console.log(dataTitle);
+      //clear the movies that were added.
+      clearMovies();
+      renderData(dataTitle.result);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
+}
 
-    const resource = `https://streaming-availability.p.rapidapi.com/search/ultra?country=us&services=netflix%2Chulu&type=movie&order_by=imdb_vote_count&page=1&genres=18%2C80&genres_relation=or&desc=true&language=en&keyword=${inputValue}&output_language=en`;
+//Search input by keyboard
 
-    //it's not going to proceed to the next line until it fetches the resource.
-    const response = await fetch(resource, options);
+function handleAddSubmitKeyboard(event) {
+  event.preventDefault();
+  const inputValue = document.getElementById("keyboard-input").value;
+  //clears the forms after it hit searches
+  document.getElementById("search-keyboard").reset();
 
-    // parsing it to json to JS data.
-    const data = await response.json();
-    console.log(data);
+  const options = {
+    method: "GET",
+    url: "https://streaming-availability.p.rapidapi.com/search/ultra",
+    params: {
+      country: "us",
+      services: "netflix,hulu,prime,disney,hbo,peacock,paramount,apple",
+      type: "movie",
+      order_by: "imdb_vote_count",
+      year_min: "2000",
+      year_max: "2020",
+      page: "1",
+      genres: "18,80",
+      genres_relation: "or",
+      desc: "true",
+      language: "en",
+      min_imdb_rating: "70",
+      max_imdb_rating: "90",
+      min_imdb_vote_count: "10000",
+      max_imdb_vote_count: "1000000",
+      keyword: `${inputValue}`,
+      output_language: "en",
+    },
+    headers: {
+      "X-RapidAPI-Key": "56fc230643msh7620609da5ab5a7p15eba2jsn84dde9e32f2d",
+      "X-RapidAPI-Host": "streaming-availability.p.rapidapi.com",
+    },
+  };
 
-    const cardContainer = document.getElementById("cardContainer");
-
-    //TODO: maybe needs a try catch
-
-    //clear the movies that were added.
-    clearMovies();
-    for (let i = 0; i < data.results.length - 1; i++) {
-      const cardCol = document.createElement("div");
-      cardCol.classList.add(
-        "col-lg-3",
-        "col-md-4",
-        "col-sm-6",
-        "d-flex",
-        "justify-content-center",
-        "mt-4"
-      );
-      cardCol.innerHTML = `<div class="card">
-      <a target="_blank" id="stream"> <img class="card-img-top" /> </a>
-    
-     <div class="card-body d-flex flex-column justify-content-between ">
-     <a id="netflix"></a>
-     <a target="_blank" id="hulu"> </a>
-
-        <h5 class="card-title"></h5>
-        <iframe id="video"></iframe>
-        <p class="card-text"></p>
-        <div>`;
-
-      const url = data.results[i].posterURLs.original;
-      const title = data.results[i].title;
-      const text = data.results[i].overview;
-      const stream = data.results[i].streamingInfo;
-
-      if (stream.hasOwnProperty("netflix")) {
-        const netflix = stream.netflix.us.link;
-        cardCol.querySelector("#netflix").setAttribute("href", netflix);
-      } else if (stream.hasOwnProperty("hulu")) {
-        const hulu = stream.hulu.us.link;
-        cardCol.querySelector("#hulu").setAttribute("href", hulu);
-      }
-
-      const video = data.results[i].video;
-
-      cardCol.querySelector(".card-title").textContent = title;
-      cardCol.querySelector(".card-text").textContent = text;
-      cardCol.querySelector(".card-img-top").setAttribute("src", url);
-      cardCol
-        .querySelector("#video")
-        .setAttribute("src", `https://www.youtube.com/embed/${video}`);
-
-      cardContainer.append(cardCol);
-    }
-  }
-} catch (err) {
-  console.error(err);
+  axios
+    .request(options)
+    .then(function (response) {
+      const dataKeyboard = response.data;
+      console.log(dataKeyboard);
+      //clear the movies that were added.
+      clearMovies();
+      renderData(dataKeyboard.results);
+    })
+    .catch(function (error) {
+      console.error(error);
+    });
 }
 
 function clearMovies() {
@@ -90,4 +97,57 @@ function clearMovies() {
   while (cardContainer.firstElementChild) {
     cardContainer.firstElementChild.remove();
   }
+}
+
+function renderData(data) {
+  data.forEach((movie) => {
+    const cardCol = document.createElement("div");
+    cardCol.classList.add(
+      "col-lg-3",
+      "col-md-4",
+      "col-sm-6",
+      "d-flex",
+      "justify-content-center",
+      "mt-4"
+    );
+    cardCol.innerHTML = `<div class="card">
+    <a target="_blank" id="stream"> <img class="card-img-top" /> </a>
+  
+   <div class="card-body d-flex flex-column justify-content-between ">
+   <a id="netflix"></a>
+   <a target="_blank" id="hulu"> </a>
+  
+      <h5 class="card-title"></h5>
+      <iframe id="video"></iframe>
+      <p class="card-text"></p>
+      <div>`;
+
+    //movie data for title = result and keyboard = results (variable that is getting from the api).
+    const url = movie.posterURLs.original;
+    const title = movie.title;
+    const text = movie.overview;
+    const stream = movie.streamingInfo;
+    const video = movie.video;
+
+    // object de-structuring
+    // const {posterURLs: {original},title,overview,streamingInfo,video} = movie
+
+    //checks if the stream has a certain services.
+    if (stream.hasOwnProperty("netflix")) {
+      const netflix = stream.netflix.us.link;
+      cardCol.querySelector("#netflix").setAttribute("href", netflix);
+    } else if (stream.hasOwnProperty("hulu")) {
+      const hulu = stream.hulu.us.link;
+      cardCol.querySelector("#hulu").setAttribute("href", hulu);
+    }
+
+    cardCol.querySelector(".card-title").textContent = title;
+    cardCol.querySelector(".card-text").textContent = text;
+    cardCol.querySelector(".card-img-top").setAttribute("src", url);
+    cardCol
+      .querySelector("#video")
+      .setAttribute("src", `https://www.youtube.com/embed/${video}`);
+    const cardContainer = document.getElementById("cardContainer");
+    cardContainer.append(cardCol);
+  });
 }
