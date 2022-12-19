3bdc3b75398dc42185a8a415dfdede96df31e257
Chase Brooks <meovermymind@gmail.com>
2022-12-19 02:23:18 -0500

    card update-style

--- js/index.js
+++ js/index.js
@@ -1,3 +1,13 @@
+const streamImage = {
+  prime: `<a class="stream_image" id="prime" title="Amazon, Public domain, via Wikimedia Commons" href="#"><img width="64" alt="Amazon Prime Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e3/Amazon_Prime_Logo.svg/64px-Amazon_Prime_Logo.svg.png"></a>`,
+  disney: `<a class="stream_image" id="disney" title="The Walt Disney Company, Public domain, via Wikimedia Commons" href="#"><img width="64" alt="Disney+ logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Disney%2B_logo.svg/512px-Disney%2B_logo.svg.png"></a>`,
+  netflix: `<a class="stream_image" id="netflix" title="Netflix, Public domain, via Wikimedia Commons" href="#"><img width="64" alt="Netflix logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/69/Netflix_logo.svg/512px-Netflix_logo.svg.png"></a>`,
+  hbo: `<a title="Warner Bros. Discovery, Public domain, via Wikimedia Commons" href="#"><img width="64" alt="HBO Max Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/1/17/HBO_Max_Logo.svg/512px-HBO_Max_Logo.svg.png"></a>`,
+  paromount: `<a title="Paramount Global, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Paramount%2B_logo.svg"><img width="64" alt="Paramount+ logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/4/4e/Paramount%2B_logo.svg/64px-Paramount%2B_logo.svg.png"></a>`,
+  hulu: `<a title="The Walt Disney Company, NBCUniversal, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:Hulu_Logo.svg"><img width="64" alt="Hulu Logo" src="https://upload.wikimedia.org/wikipedia/commons/thumb/e/e4/Hulu_Logo.svg/512px-Hulu_Logo.svg.png"></a>`,
+  youtube: `<a id="video" title="Original: YouTube Vector:  Jarould, Public domain, via Wikimedia Commons" href="https://commons.wikimedia.org/wiki/File:YouTube_full-color_icon_(2017).svg"><img width="32" alt="YouTube full-color icon (2017)" src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/YouTube_full-color_icon_%282017%29.svg/128px-YouTube_full-color_icon_%282017%29.svg.png"></a>`,
+};
+
 //Get by Title Movies
 document
   .getElementById("search-title")
@@ -232,12 +242,18 @@ function renderData(data) {
             <div class="card__content">
               <div class="card__header">
                 <h5 class="card-title"></h5>
-                <a  target="_blank" id="video" href="#"><i class="bi bi-youtube"></i></a> 
+                ${streamImage.youtube} 
               </div>
               <div class="card__body">
-                
-        
         <p class="card-text"></p>
+        <div class="streams">
+        ${streamImage.hulu}
+        ${streamImage.prime}
+        ${streamImage.disney}
+        ${streamImage.netflix}
+        ${streamImage.hbo}
+        ${streamImage.paromount}
+                </div>
               </div>
             </div>
           </div>
