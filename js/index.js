92aeaad5780177a4444488ec0a202f647e032394
mrjoelee <joe.05.lee@outlook.com>
2022-12-20 15:19:50 -0500

    movie-curtain

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ async function displayNewPage(e) {
 
 async function handleAddSubmitTitle(event) {
   event.preventDefault();
-
+  showTime();
   const inputTitle = document.getElementById("title-input").value;
   //clears the forms after it hit searches
   document.getElementById("search-title").reset();
@@ -64,6 +64,7 @@ async function handleAddSubmitTitle(event) {
 
 async function handleAddSubmitKeyboard(event) {
   event.preventDefault();
+  showTime();
   const inputValue = document.getElementById("keyboard-input").value;
   //clears the forms after it hit searches
   document.getElementById("search-keyboard").reset();
@@ -250,7 +251,7 @@ function renderData(data) {
 
       movieCol.querySelector(".card-title").textContent = title;
       movieCol.querySelector(".card-text").textContent = text;
-      const favMovie = movieCol.querySelector('[button-id]');
+      const favMovie = movieCol.querySelector("[button-id]");
       cardContainer.append(movieCol);
 
       //toggles the flip method
@@ -264,17 +265,17 @@ function renderData(data) {
       favMovie.addEventListener("click", (e) => {
         e.preventDefault();
         e.stopPropagation();
-        let btnId = parseInt(favMovie.getAttribute('button-id'));  
-       
+        let btnId = parseInt(favMovie.getAttribute("button-id"));
+
         if (btnId === movie.tmdbId) {
           favorites.push(movie);
           localStorage.setItem("favorites", JSON.stringify(favorites));
         }
       });
 
-        // console.log(document.querySelector('button[data-id]'));
-        favMovie.addEventListener('click', e => {
-        e.preventDefault(); 
+      // console.log(document.querySelector('button[data-id]'));
+      favMovie.addEventListener("click", (e) => {
+        e.preventDefault();
         favMovie.innerHTML = `<div button-id="${movie.tmdbId} class="_abm0"><span class="_aame"><svg aria-label="Unlike" class="_ab6-" color="#ed4956" fill="#ed4956" height="24" role="img" viewBox="0 0 48 48" width="24"><path d="M34.6 3.1c-4.5 0-7.9 1.8-10.6 5.6-2.7-3.7-6.1-5.5-10.6-5.5C6 3.1 0 9.6 0 17.6c0 7.3 5.4 12 10.6 16.5.6.5 1.3 1.1 1.9 1.7l2.3 2c4.4 3.9 6.6 5.9 7.6 6.5.5.3 1.1.5 1.6.5s1.1-.2 1.6-.5c1-.6 2.8-2.2 7.8-6.8l2-1.8c.7-.6 1.3-1.2 2-1.7C42.7 29.6 48 25 48 17.6c0-8-6-14.5-13.4-14.5z"></path></svg></span></div>`;
       });
     });
@@ -359,3 +360,21 @@ function getFavorites() {
   }
 }
 
+document.body.addEventListener("onload", focus());
+function showTime() {
+  const curtain = document.getElementById("curtain");
+  curtain.className = "open";
+
+  const scene = document.getElementById("scene");
+  scene.className = "expand";
+
+  setTimeout(function () {
+    scene.style.display = "none";
+  }, 5000);
+}
+
+function deleteCurtain() {
+  setTimeout(function () {
+    clearChildrenElement(scene);
+  }, 5000);
+}
